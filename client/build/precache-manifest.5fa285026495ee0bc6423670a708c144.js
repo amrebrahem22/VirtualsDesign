1a26ed96e2438a996daab87d698bf72e30bb42a1
amrebrahem22 <amrebrahem226@gmail.com>
2021-11-02 05:21:25 +0200

    heroku

---
+++ client/build/precache-manifest.5fa285026495ee0bc6423670a708c144.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602de3c30ccd8bd56df4753e57ceecd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d88cb3bbf54ce3e0b35",
+    "url": "/static/css/2.469a25eb.chunk.css"
+  },
+  {
+    "revision": "349d54679a6fda3ab6bd",
+    "url": "/static/css/main.cf143f0d.chunk.css"
+  },
+  {
+    "revision": "0d88cb3bbf54ce3e0b35",
+    "url": "/static/js/2.4b2e954c.chunk.js"
+  },
+  {
+    "revision": "349d54679a6fda3ab6bd",
+    "url": "/static/js/main.a8c0aec1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e1b4db725534655d6d4c20e56b4fd0fc",
+    "url": "/static/media/avatar.e1b4db72.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "90c3aece2e05d9fadb269147e5682acd",
+    "url": "/static/media/virtualsdesign-logo.90c3aece.png"
+  }
+]);
